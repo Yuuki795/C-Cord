c0e0557e1178c0b8705cfe2e6733ec3f20e1fdca
Yuuki <Yuuki@DESKTOP-VFN4VAF>
2022-11-26 11:33:40 -0500

    zzz

---
+++ C-Cord/MainWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+
+
+namespace CCord
+{
+
+
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+
+        public ChromiumWebBrowser browser;
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("https://discord.com/login");
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
